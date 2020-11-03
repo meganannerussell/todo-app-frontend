2c6f8160c8972b1843e67b766f83ea8351fbf7bb
Megan Russell <meganannerussell@gmail.com>
2020-11-03 15:07:23 +0200

    added initial todos

--- src/App.js
+++ src/App.js
@@ -10,10 +10,16 @@ const env = runtimeEnv();
 export const apiBaseUrl = env.REACT_APP_API_URL || "http://localhost:5000";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { description: "wash my car", checked: false },
+    {
+      description: "do work",
+      checked: false,
+    },
+  ]);
 
   return (
-    <Fragment style={{height: "100%", backgroundColor:'blue'}}>
+    <Fragment style={{ height: "100%", backgroundColor: "blue" }}>
       {/* <img style={{ position: "absolute", height: "100%", width: '100%' }} src={wave} /> */}
       <div className="outerContainer">
         <div className="inputContainer">
